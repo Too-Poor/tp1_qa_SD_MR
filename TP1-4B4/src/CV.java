60669165e871cfc78ce14049d231d0b103611373
Salif Diarra <1821732@cmontmorency.qc.ca>
2020-02-03 08:59:45 -0500

    Création du constructeur de CV.

--- TP1-4B4/src/CV.java
+++ TP1-4B4/src/CV.java
@@ -6,9 +6,23 @@ public class CV {
 	private String formation;
 	private int annees_experience;
 	private String[] competences;
-	private String[] attentes;
+	private String attentes;
+	
+	public CV (String nom, String prenom, String formation,
+			int annees_experience, String[] competences, String attentes) {
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = prenom;
+		this.annees_experience = annees_experience;
+		this.competences = competences;
+		this.attentes = attentes;
+		
+	}
 	
 	public static void main( String[] args ) {
+		System.out.println( "Bienvenue chez Barette!" );
+		
 		
 	}
 
