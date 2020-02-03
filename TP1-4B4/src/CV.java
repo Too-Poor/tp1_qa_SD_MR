9c367024ac73d80d6e908d9e5027f85af61ba4de
Salif Diarra <1821732@cmontmorency.qc.ca>
2020-02-03 08:52:57 -0500

    Création des champs d'instance dans la classe CV.

---
+++ TP1-4B4/src/CV.java
@@ -0,0 +1,15 @@
+
+public class CV {
+
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int annees_experience;
+	private String[] competences;
+	private String[] attentes;
+	
+	public static void main( String[] args ) {
+		
+	}
+
+}
